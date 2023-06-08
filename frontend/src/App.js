0639bc37a417850dfebad58637e3e9cb205f868a
Kshitij <girkarkshitij@gmail.com>
2023-06-08 23:29:54 +0530

    feat: add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Header from './components/Header';
+import {Container} from 'react-bootstrap';
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to TechShop</h1>
+        </Container>
+      </main>
+    </div>
+  );
 }
 
 export default App;
