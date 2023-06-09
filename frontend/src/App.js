8715782a61c5463857762f2420fcb161471072e1
Kshitij <girkarkshitij@gmail.com>
2023-06-09 16:50:50 +0530

    feat: add Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import {Container} from 'react-bootstrap';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <h1>Welcome to TechShop</h1>
         </Container>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+
+function Footer() {
+  return (
+    <Container>
+      <footer className='text-center py-3'>
+        <p>&copy; Kshitij Girkar, 2023</p>
+      </footer>
+    </Container>
+  );
+}
+
+export default Footer;
