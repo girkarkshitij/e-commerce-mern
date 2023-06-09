65c4672ed028ffdb008a0dbcedbf176e3265ee89
Kshitij <girkarkshitij@gmail.com>
2023-06-09 18:24:57 +0530

    fix: replace hardcoded year in footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Container} from 'react-bootstrap';
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <Container>
       <footer className='text-center py-3'>
-        <p>&copy; Kshitij Girkar, 2023</p>
+        <p>&copy; Kshitij Girkar, {currentYear}</p>
       </footer>
     </Container>
   );
