9af51641bc127507cb97c5e87cd8cb7882843a21
Kshitij Girkar <girkarkshitij@gmail.com>
2023-06-21 23:05:07 +0530

    add: function to connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 
 const app = express();
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
