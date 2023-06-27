482b68bbd5c1782b6f4779bc86fe6fb582431783
Kshitij Girkar <girkarkshitij@gmail.com>
2023-06-27 16:46:14 +0530

    update: mongodb connect output

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 async function connectDB() {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`######### Mongodb connected to ${conn.connection.host} ###########`);
+    console.log(`✅✅✅✅✅✅ Mongodb connected to ${conn.connection.host} ✅✅✅✅✅✅`);
   } catch (error) {
     console.log(error);
   }
