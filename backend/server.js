c303bad4eba1d1c6c0aa66ba749f8a19bb1e406e
Kshitij Girkar <girkarkshitij@gmail.com>
2023-09-03 18:47:04 +0530

    add: body parser middleware

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ import userRoutes from "./routes/userRoutes.js";
 const app = express();
 connectDB();
 
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
