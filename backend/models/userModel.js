509c20e8ace97254647a4d84f11c9fc0a598acb8
Kshitij Girkar <girkarkshitij@gmail.com>
2023-09-03 18:47:32 +0530

    add: match password method to user schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,21 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-}, {timestamps: true});
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', UserSchema);
+UserSchema.methods.matchPassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
+
+const User = mongoose.model("User", UserSchema);
 
 export default User;
