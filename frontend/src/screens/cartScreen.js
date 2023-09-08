2363bc14aca6583c359222637a0919004ec7a9bd
Kshitij Girkar <girkarkshitij@gmail.com>
2023-09-08 22:25:22 +0530

    fix: proceed to checkout button not working

--- frontend/src/screens/cartScreen.js
+++ frontend/src/screens/cartScreen.js
@@ -114,7 +114,7 @@ const CartScreen = () => {
                 type="button"
                 className="btn-block"
                 disabled={cartItems.length === 0}
-                onClick={checkoutHandler()}
+                onClick={checkoutHandler}
               >
                 Proceed To Checkout
               </Button>
