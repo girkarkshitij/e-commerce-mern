3401bc838260e49783eb9e540bdc2e4e0b534ff8
Kshitij Girkar <girkarkshitij@gmail.com>
2023-09-13 23:13:08 +0530

    add: screen for shipping

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import FormContainer from "../components/FormContainer";
+import { saveShippingAddress } from "../slices/cartSlice";
+
+function ShippingScreen() {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const [address, setAddress] = useState(shippingAddress?.address || "");
+  const [city, setCity] = useState(shippingAddress?.city || "");
+  const [pincode, setPincode] = useState(shippingAddress?.pincode || "");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  function submitHandler(e) {
+    e.preventDefault();
+    dispatch(saveShippingAddress({ address, city, pincode }));
+    navigate("/payment");
+  }
+
+  return (
+    <FormContainer>
+      <h1>Shipping</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="address" className="my-2">
+          <Form.Label>Address</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="city" className="my-2">
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="pincode" className="my-2">
+          <Form.Label>Pincode</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your pincode"
+            value={pincode}
+            onChange={(e) => setPincode(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary" className="my-2">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+}
+
+export default ShippingScreen;
