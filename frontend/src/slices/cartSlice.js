74c716ba86764ce88179e5199b510299b88b30eb
Kshitij Girkar <girkarkshitij@gmail.com>
2023-09-15 23:34:34 +0530

    fix: provide default payment method

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: [], shippingAddress: {}, paymentMethod: "" };
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "UPI" };
 
 const cartSlice = createSlice({
   name: "cart",
