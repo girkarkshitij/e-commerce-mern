770c5995a92245a633e3231a72e0795a43359913
Kshitij Girkar <girkarkshitij@gmail.com>
2023-09-16 13:23:45 +0530

    feat: clear cart items function

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -48,6 +48,10 @@ const cartSlice = createSlice({
       state.paymentMethod = action.payload;
       localStorage.setItem("cart", JSON.stringify(state));
     },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      localStorage.setItem("cart", {});
+    },
   },
 });
 
@@ -56,6 +60,7 @@ export const {
   removeFromCart,
   saveShippingAddress,
   savePaymentMethod,
+  clearCartItems,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
