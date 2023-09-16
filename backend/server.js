00262f21b67afabce06a84cb8b56712bbd8cb19f
Kshitij Girkar <girkarkshitij@gmail.com>
2023-09-16 18:16:36 +0530

    deploy code changes

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,14 @@ app.use(express.urlencoded({ extended: true }));
 // cookie parser middleware
 app.use(cookieParser());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
