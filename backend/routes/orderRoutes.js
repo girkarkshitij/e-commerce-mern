95793d536cfe94c56cf246abf29f7e4da013ddac
Kshitij Girkar <girkarkshitij@gmail.com>
2023-09-16 13:12:30 +0530

    add order items route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -9,14 +9,35 @@ import { protect } from "../middleware/authMiddleware.js";
 // @route   POST /api/orders
 // @access  Private
 router.post("/", protect, async (req, res) => {
-  res.send("add order items");
+  const { orderItems, shippingAddregss, paymentMethod, totalPrice } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400).json({ msg: "No order items" });
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddregss,
+      paymentMethod,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
 });
 
 // @desc    Get logged in user orders
 // @route   GET /api/orders/mine
 // @access  Private
 router.get("/mine", protect, async (req, res) => {
-  res.send("get my orders");
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // @desc    Update order as paid
